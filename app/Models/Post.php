ee9e2ecd26c88e4cb9c2e1392a32ac5dd59c29f5
Laurin49 <welter@diwe.info>
2023-03-02 16:18:12 +0100

    section 4 work with databases o.k.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,51 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use \Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
+    use HasFactory;
+    // protected $guarded = ['id'];
+    protected $fillable = ['title', 'excerpt', 'body'];
 
-    public $title;
-    public $excerpt;
-    public $date;
-    public $body;
-    public $slug;
-    public function __construct($title, $excerpt, $date, $body, $slug)
+    // eager loading, lädt category und author immer mit
+    // protected $with = ['category', 'author'];
 
-    {
-        $this->title = $title;
-        $this->excerpt = $excerpt;
-        $this->date = $date;
-        $this->body = $body;
-        $this->slug = $slug;
+    public function category() {
+        return $this->belongsTo(Category::class);
     }
 
-
-    public static function all() {
-        return cache()->rememberForever('posts.all', function () {
-            return $posts = collect(File::files(resource_path('posts')))
-                ->map(function($file) {
-                    $document = YamlFrontMatter::parseFile($file);
-                    return new Post(
-                        $document->title, $document->excerpt, $document->date, $document->body(), $document->slug
-                    );
-                })
-                ->sortByDesc('date');
-        });
-    }
-
-    public static function find($slug) {
-        return static::all()->firstWhere('slug', $slug);
-    }
-
-    public static function findOrFail($slug) {
-        $post = static::find($slug);
-        if (! $post) {
-            throw new ModelNotFoundException();
-        }
-        return $post;
+    public function author() {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
