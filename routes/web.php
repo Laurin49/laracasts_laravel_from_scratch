13806f2915935242f2a03b9049e4427722b466e4
Laurin49 <welter@diwe.info>
2023-03-07 13:00:59 +0100

    section6_search o.k.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use App\Models\Post;
 use App\Models\Category;
 use App\Models\User;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+
 use Illuminate\Support\Facades\DB;
 /*
 |--------------------------------------------------------------------------
@@ -16,22 +18,10 @@ use Illuminate\Support\Facades\DB;
 |
 */
 
-Route::get('/', function () {
-    // Log query - Aufrufe
-//    DB::listen(function($query) {
-//        logger($query->sql, $query->bindings);
-//    });
-    return view('posts', [
-        'posts' => Post::latest()->with('category', 'author')->get(),
-        'categories' => Category::all()
-    ]);
-})->name('home');
+Route::get('/', [PostController::class, 'index'])->name('home');
 
-Route::get('posts/{post:slug}', function(Post $post) {
-    return view('post')->with([
-        'post' => $post
-    ]);
-});
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
 Route::get('categories/{category:slug}', function(Category $category) {
     return view('posts', [
         'posts' => $category->posts->load(['category', 'author']),
